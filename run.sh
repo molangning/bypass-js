d7cc87eefbfd2401f7a432862e70b307be6aeccb
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 10:09:41 +0000

    final update istg if it doesn’t work

--- run.sh
+++ run.sh
@@ -52,10 +52,11 @@ function confirm() {
 git ls-remote -q > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-  git remote add origin https://github.com/molangning/bypass-js.git
-  git pull
-  git branch --set-upstream-to=origin/main main
-  git pull
+git init
+git remote add origin https://github.com/molangning/bypass-js.git
+git fetch
+git reset origin/main
+git checkout -t origin/main
 fi
 
 
