46c8e14da584046bd8fb83849d3bed7ea8e7d2de
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 10:13:12 +0000

    added comments of gratitude

--- run.sh
+++ run.sh
@@ -51,6 +51,8 @@ function confirm() {
 
 git ls-remote -q > /dev/null 2>&1
 
+# this helped me save so much time
+# https://stackoverflow.com/a/18999726
 if [ $? -ne 0 ]; then
   git init
   git remote add origin https://github.com/molangning/bypass-js.git
