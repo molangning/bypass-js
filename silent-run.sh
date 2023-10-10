7bbede87d49c074451a0bbe18e52c6981d9be13f
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 09:57:24 +0000

    I’m really tired of fixing this

--- silent-run.sh
+++ silent-run.sh
@@ -4,8 +4,8 @@ git ls-remote -q > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
   git remote add origin https://github.com/molangning/bypass-js.git
+  git pull
   git branch --set-upstream-to=origin/main main
-  git checkout main
   git pull
 fi
 
