91813a24e0136f0fc3f24a24d1ab7b1d828de61b
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 09:53:24 +0000

    I’m tired, boss

--- run.sh
+++ run.sh
@@ -53,6 +53,7 @@ git ls-remote -q > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
   git remote add origin https://github.com/molangning/bypass-js.git
+  git branch --set-upstream-to=origin/main main
   git checkout main
   git pull
 fi
