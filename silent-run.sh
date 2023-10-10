08b6b8d1f16c75a45dddb3510699465ba917d7c3
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 09:19:39 +0000

    improved support for a particularly stubborn hosting provider that I like

--- silent-run.sh
+++ silent-run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -d ".git" ]; then
+  git remote add origin https://github.com/molangning/bypass-js.git
+  git checkout main
+  git pull
+fi
+
 npm install express --save
 git pull
 node server.js
