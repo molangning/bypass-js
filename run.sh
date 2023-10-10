789431ea9437aa4a8dd8b3520704404b8399be5e
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 10:11:03 +0000

    indentation

--- run.sh
+++ run.sh
@@ -52,11 +52,11 @@ function confirm() {
 git ls-remote -q > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-git init
-git remote add origin https://github.com/molangning/bypass-js.git
-git fetch
-git reset origin/main
-git checkout -t origin/main
+  git init
+  git remote add origin https://github.com/molangning/bypass-js.git
+  git fetch
+  git reset origin/main
+  git checkout -t origin/main
 fi
 
 
