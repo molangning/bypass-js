066da85ac8e9cd8311fa009b2f3e1729cd1196a8
molangning <133737702+molangning@users.noreply.github.com>
2023-10-10 09:38:58 +0000

    pls be fix

--- run.sh
+++ run.sh
@@ -49,7 +49,9 @@ function confirm() {
 
 }
 
-if [ ! -d ".git" ]; then
+git ls-remote -q 2> /dev/null
+return_code
+if [ $return_code -ne 0 ]; then
   git remote add origin https://github.com/molangning/bypass-js.git
   git checkout main
   git pull
--- silent-run.sh
+++ silent-run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ".git" ]; then
+git ls-remote -q 2> /dev/null
+return_code
+if [ $return_code -ne 0 ]; then
   git remote add origin https://github.com/molangning/bypass-js.git
   git checkout main
   git pull
 fi
+
 
 npm install express --save
 git pull
